5556c2a900aee3ca48f41417f859bb84a62fc565
Yadav106 <sahil.jp.yadav@gmail.com>
2024-01-04 08:24:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Timer from './components/Timer'
+
+const App = () => {
+  return (
+    <div className='bg-gray-700 flex justify-center items-center h-[100vh]'>
+      <Timer />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Timer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Timer = () => {
+  return (
+    <div>
+        <div className='h-[100px] w-[100px] bg-gray-500'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Timer
